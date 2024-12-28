717948c90497ad786a9633eb354f183b5a1fe6a1
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-12-28 12:04:32 +0000

    expo router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 //Definle global providers
 
-import { Slot, Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,9 @@
+import { Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeSreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Redirect href={"/(home)/(tabs)"} />;
 }
 
 const styles = StyleSheet.create({
