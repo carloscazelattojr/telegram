08db1fc7777bbd68683e35e13618a301cc15760a
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-12-28 12:28:21 +0000

    install Stream chat SDK

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,12 @@
 //Definle global providers
 
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 }
