00546dbe12fa3288fc65fc93acbf80920b9835c4
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-12-30 01:50:20 +0000

    user supabase chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { ChannelList } from "stream-chat-expo";
 import { router } from "expo-router";
+import { useAuth } from "../../../providers/AuthProvider";
 
 export default function MainTabScreen() {
+  const { user } = useAuth();
+
   return (
     <ChannelList
+      filters={{ members: { $in: [user?.id] } }}
       onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
     />
   );
