408859d23ead02da84ad6a68eea51cef81bb7a35
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-12-30 03:28:20 +0000

    start channel message

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,14 +1,31 @@
 import { ChannelList } from "stream-chat-expo";
-import { router } from "expo-router";
+import { Link, router, Stack } from "expo-router";
 import { useAuth } from "../../../providers/AuthProvider";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function MainTabScreen() {
   const { user } = useAuth();
 
   return (
-    <ChannelList
-      filters={{ members: { $in: [user?.id] } }}
-      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-    />
+    <>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <Link href={"/(home)/users"} asChild>
+              <FontAwesome5
+                name="users"
+                size={24}
+                color="gray"
+                style={{ marginHorizontal: 25 }}
+              />
+            </Link>
+          ),
+        }}
+      />
+      <ChannelList
+        filters={{ members: { $in: [user?.id] } }}
+        onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+      />
+    </>
   );
 }
---
+++ src/components/UserListItem.tsx
@@ -0,0 +1,28 @@
+import { Text, Pressable } from "react-native";
+import { useChatContext } from "stream-chat-expo";
+import { useAuth } from "../providers/AuthProvider";
+import { router } from "expo-router";
+
+const UserListItem = ({ user }) => {
+  const { client } = useChatContext();
+  const { user: me } = useAuth();
+
+  const onPress = async () => {
+    const channel = client.channel("messaging", {
+      members: [me.id, user.id],
+    });
+    await channel.create();
+    router.replace(`/(home)/channel/${channel.cid}`);
+  };
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={{ padding: 16, backgroundColor: "white" }}
+    >
+      <Text style={{ fontWeight: "600" }}>{user.full_name}</Text>
+    </Pressable>
+  );
+};
+
+export default UserListItem;
